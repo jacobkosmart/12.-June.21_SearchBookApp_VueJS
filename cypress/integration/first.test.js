60ab00dac5839acef4e9f6cd6d7f95b33f8dad8d
jacobkosmart <jacobkosmart@gmail.com>
2021-06-24 16:14:24 +0900

    add testing

---
+++ cypress/integration/first.test.js
@@ -0,0 +1,5 @@
+describe('First page', () => {
+  it('move project page', () => {
+    cy.visit('http://localhost:8080')
+  })
+})
--- tests/example.test.js
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@jest/globals'
-import { mount } from '@vue/test-utils'
-import Example from './Example.vue'
-
-test('메세지를 변경합니다', () => {
-  const wrapper = mount(Example)
-  expect(wrapper.vm.msg).toBe('Hello Vue Test utils')
-})
-
